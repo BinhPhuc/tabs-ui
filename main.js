416e440aad0d1eb8b3a7845dcf9f101ff629f3c9
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2023-03-19 14:48:25 +0700

    done

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 const tabs = document.querySelectorAll('.tab-item');
 const tabText = document.querySelectorAll('.tab-des-item');
 const line = document.querySelector('.line');
+const iconList = document.querySelectorAll('.tab-icon')
+
+const curTabActive = document.querySelector('.tab-item.active');
+
+line.style.left = curTabActive.offsetLeft + 20 + "px";
+line.style.width = curTabActive.offsetWidth + "px";
 
 for(var i = 0; i < tabs.length; i++) {
     const pane = tabText[i];
     const tab = tabs[i];
-    tabs[i].addEventListener('click', function(e) {
+    const icon = iconList[i];
+    tab.addEventListener('click', function(e) {
         var curTabActive = document.querySelector('.tab-item.active');
+        var curIconActive = document.querySelector('.tab-icon.active');
         var curPaneActive = document.querySelector('.tab-des-item.active');
-        curTabActive.classList.remove('active');
+        if(curTabActive.classList.contains('active')) {
+            curTabActive.classList.remove('active');
+        }
+        else {
+            curIconActive.classList.remove('active');
+        }
         curPaneActive.classList.remove('active');
-        e.target.classList.add('active');
+        tab.classList.add('active');
+        icon.classList.add('active');
         pane.classList.add('active');
         line.style.left = this.offsetLeft + 20 + "px";
         line.style.width = this.offsetWidth + "px";
